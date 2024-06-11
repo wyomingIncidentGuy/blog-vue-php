260910710b01a32e31f5413ad6f086ccfc6d3e6e
Wawg34345 <andrey.dar1214@gmail.com>
2024-06-11 20:51:07 +0300

    fixed for windows

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     devServer: {
-        proxy: 'http://localhost:8080'
+        proxy: 'http://localhost:8080',
+        watchOptions: {
+            poll: true,
+        },
+        hot: true,
     }
 };
